331dd176238b4786a4de8000c7676179e393256b
Ignacio-Montaldi <ignaciomontaldi@gmail.com>
2022-06-14 11:49:35 -0300

    Test

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ app.listen(app.get("port"), () => {
 app.set("views", path.resolve(__dirname, "views"))
 app.use(express.static(path.resolve(__dirname, "../public")))
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 // Routes
 
